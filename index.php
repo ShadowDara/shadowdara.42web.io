301df90acc56568dc9d4f1a18fa68d78ca4f9312
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-11-25 23:14:22 +0100

    so

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
-<?php 
-// NO PHP here
-?>
+<!--
+
+php -S localhost:8080
+
+-->
 
 <!DOCTYPE html>
 
 <html>
 <head>
   <meta http-equiv="CONTENT-TYPE" content="text/html; charset=UTF-8">
+  <link rel="stylesheet" href="index.css">
   <title>Index</title>
 </head>
 <body>
-  <h1>Empty here</h1>
+  <h1>Very Empty here</h1>
+  <div>
+    <h2>Projects</h2>
+    <ul>
+      <li><a href="https://github.com/shadowdara/finder">finder</a></li>
+    </ul>
+  </div>
   <p>More Content available <a href="https://shadowdara.github.io">here</a></p>
+  <img src="https://media1.tenor.com/m/x8v1oNUOmg4AAAAd/rickroll-roll.gif">
 </body>
 </html>
